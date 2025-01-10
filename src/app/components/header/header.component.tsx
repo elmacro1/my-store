d85a59925b203b33bdf0795b9e111b64775adab3
Marco Antonio Galvan Fernandez <elmacro11@gmail.com>
2025-01-10 17:50:16 -0300

    add slide menu

--- src/app/components/header/header.component.tsx
+++ src/app/components/header/header.component.tsx
@@ -1,35 +1,16 @@
 import Link from "next/link";
-import Image from "next/image";
 
 import SearchbarComponent from "../common/searchbar/searchbar.component";
 import NavbarComponent from "../navbar/navbar.component";
-import shoppingCartIcon from "@/assets/icons/shopping-cart.svg";
-import burgerIcon from "@/assets/icons/burger-menu.svg";
 import styles from "./header.module.css";
 
 const HeaderComponent = () => (
   <header className={styles.main_container}>
     <div className={styles.content_container}>
-      <Image
-        src={burgerIcon}
-        alt="burger icon"
-        width={20}
-        height={20}
-        className={styles.burger_icon}
-      />
       <Link href="/" className={styles.title}>
         OM
       </Link>
-      <div className={styles.searchbar_container}>
-        <SearchbarComponent />
-      </div>
-      <Image
-        src={shoppingCartIcon}
-        alt="shopping icon"
-        className={styles.shopping_cart_icon}
-        width={24}
-        height={24}
-      />
+      <SearchbarComponent />
       <NavbarComponent />
     </div>
   </header>
--- src/app/components/navbar/navbar.component.tsx
+++ src/app/components/navbar/navbar.component.tsx
@@ -1,29 +1,62 @@
 "use client";
 
+import { useState } from "react";
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 import { navItems } from "@/constants/navbar";
+import burgerIcon from "@/assets/icons/burger-menu.svg";
+import shoppingCartIcon from "@/assets/icons/shopping-cart.svg";
 import styles from "./navbar.module.css";
 
 const NavbarComponent = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const pathname = usePathname();
+
+  const openSlideMenu = () => setIsOpen(true);
+  const closeSlideMenu = () => setIsOpen(false);
 
   return (
     <nav className={styles.main_container}>
-      {navItems.map(({ name, route }) => {
-        return (
-          <Link
-            key={name}
-            href={route}
-            className={`${styles.item} ${
-              pathname === route ? styles.active : ""
-            }`}
-          >
-            {name}
-          </Link>
-        );
-      })}
+      <div className={`${styles.overlay} ${isOpen ? styles.menu_open : ""}`}>
+        <div
+          className={`${styles.nav_container} ${
+            isOpen ? styles.nav_container_open : ""
+          }`}
+        >
+          {navItems.map(({ name, route }) => (
+            <Link
+              key={name}
+              href={route}
+              className={`${styles.item} ${
+                pathname === route ? styles.active : ""
+              }`}
+              onClick={closeSlideMenu}
+            >
+              {name}
+            </Link>
+          ))}
+        </div>
+        <button className={styles.close_button} onClick={closeSlideMenu}>
+          Cerrar
+        </button>
+      </div>
+      <Image
+        src={shoppingCartIcon}
+        alt="shopping icon"
+        className={styles.shopping_cart_icon}
+        width={24}
+        height={24}
+      />
+      <Image
+        src={burgerIcon}
+        alt="burger icon"
+        width={20}
+        height={20}
+        className={styles.burger_icon}
+        onClick={openSlideMenu}
+      />
     </nav>
   );
 };
