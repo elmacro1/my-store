f212fca02e60561ac396d496e14c14ad427953d1
Marco Antonio Galvan Fernandez <elmacro11@gmail.com>
2025-01-24 18:02:15 -0300

    add filterbar and sortbar

--- src/app/productos/page.tsx
+++ src/app/productos/page.tsx
@@ -2,14 +2,23 @@ import FilterBar from "@/components/filter-bar/filter-bar";
 import ProductList from "@/components/product-list/product-list";
 import styles from "./page.module.css";
 
-const ProductosPage = () => (
-  <main className={styles.main_container}>
-    <div className={styles.content_container}>
-      <FilterBar />
-      <ProductList />
-      <FilterBar />
-    </div>
-  </main>
-);
+const ProductosPage = () => {
+  const categories = ["Todas", "Hogar", "Tecnología", "Moda", "Deportes"];
+  const orderBy = [
+    "Más recientes",
+    "Precio: Menor a mayor",
+    "Precio: Mayor a menor",
+  ];
+
+  return (
+    <main className={styles.main_container}>
+      <div className={styles.content_container}>
+        <FilterBar filters={categories} tag="categorie" title="Categorias" />
+        <ProductList />
+        <FilterBar filters={orderBy} tag="sort" title="Ordenar por" />
+      </div>
+    </main>
+  );
+};
 
 export default ProductosPage;
--- src/components/filter-bar/filter-bar.tsx
+++ src/components/filter-bar/filter-bar.tsx
@@ -1,16 +1,47 @@
+"use client";
+
+import { useSearchParams, useRouter, usePathname } from "next/navigation";
+
 import styles from "./filter-bar.module.css";
 
-const FilterBar = () => (
-  <aside className={styles.main_container}>
-    <p className={styles.filter_title}>Categorías</p>
-    <ul className={styles.filter_list}>
-      <li className={styles.filter_item}>Tops</li>
-      <li className={styles.filter_item}>Remeras</li>
-      <li className={styles.filter_item}>Calzas cortas</li>
-      <li className={styles.filter_item}>Calzas largas</li>
-      <li className={styles.filter_item}>Corpiños</li>
-    </ul>
-  </aside>
-);
+interface Props {
+  title: string;
+  filters: string[];
+  tag: string;
+}
+
+const FilterBar = ({ title, filters, tag }: Props) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const params = new URLSearchParams(searchParams);
+
+  const handleClick = (item: string) => {
+    const formattedItem = item.toLowerCase();
+    console.log({ formattedItem });
+
+    params.set(tag, formattedItem);
+    router.push(`${pathname}?${params.toString()}`);
+  };
+
+  return (
+    <aside className={styles.main_container}>
+      <p className={styles.filter_title}>{title}</p>
+      <ul className={styles.filter_list}>
+        {filters.map((item, index) => (
+          <li
+            className={`${styles.filter_item} ${
+              params.get(tag)?.includes(item.toLowerCase()) ? styles.active : ""
+            }`}
+            key={index}
+            onClick={() => handleClick(item)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+};
 
 export default FilterBar;
